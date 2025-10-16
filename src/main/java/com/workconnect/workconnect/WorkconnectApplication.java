7c27512bb7834faffe72f7372e86f1c520e4676b
maka <castillolopezmario96@gmail.com>
2025-10-16 20:24:21 +0200

    first commit

---
+++ src/main/java/com/workconnect/workconnect/WorkconnectApplication.java
@@ -0,0 +1,13 @@
+package com.workconnect.workconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workconnect/workconnect/WorkconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workconnect.workconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
